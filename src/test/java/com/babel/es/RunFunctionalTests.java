0489dc2dcc916a17aeffa101cdcc8544f9f71d8f
Khushiak48 <khushiak48@gmail.com>
2017-07-11 20:57:19 +0100

    runFunctionalFile

--- src/test/java/com/babel/es/RunFunctionalTests.java
+++ src/test/java/com/babel/es/RunFunctionalTests.java
@@ -16,14 +16,15 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(format = { "pretty", "json:target/cucumber-json" },
 		// features = {"classpath:functional/babel.feature:63",
 		// "classpath:functional/babel2.feature:36" },
-		features = "classpath:functional/babel.feature:63", glue = {"com.babel.es"})
+		features = "classpath:functional/babel.feature:9", glue = {"com.babel.es"})
+	    // tags = "@Radio"	
 
 public class RunFunctionalTests extends AbstractTestNGCucumberTests {
 
 	@Parameters("browsername")
 	@BeforeClass
-	public void parameterTest(String browsername) {
-		BabelStepDefs.browserType = browsername;
-		System.out.println("Parameterized value is : " + browsername);
+	public void parameterTest(String namebrowser) {
+		BabelStepDefs.browserType = namebrowser;
+		System.out.println("Parameterized value is : " + namebrowser);
 	}
 }
